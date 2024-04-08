a8b2bdf19e1cfae8e59659155467b9ec4d062ad2
Luis Beqja <luisbeqjamw@gmail.com>
2024-04-08 21:55:15 +0200

    remove be project

--- web-sense-backend/lang_chain/PydanticModels.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, Field
-
-# Define the Pydantic model that will be used to parse the output
-class AnswerInfo(BaseModel):
-    title: str = Field(description="Title of the answer")
-    text: int = Field(description="Helpful Answer")
--- web-sense-backend/openai_api/setup.py
+++
@@ -1,6 +0,0 @@
-import os
-from openai import OpenAI
-
-client = OpenAI(
-    api_key = os.environ.get('OPENAI_API_KEY')
-)
