610332e9edd5f4e54546e36a14e6e97d46e9c084
luis beqja <luisbeqjamw@gmail.com>
2024-11-20 13:15:41 +0100

    Fix BE url

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,1 @@
-export const SERVER_URL = location.hostname === "localhost" ? "http://localhost:5000" : "https://websense-backend-production.up.railway.app";
+export const SERVER_URL = location.hostname === "localhost" ? "http://localhost:5000" : "https://back.websense-ai.com";
